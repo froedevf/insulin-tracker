b68a86c3d1602d5fc6fd63dcdd19fe433c069ad5
froedevf <david_l_katz@yahoo.com>
2021-02-22 19:55:52 -0500

    Fix indent

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 	# Load current count
-	f = open("count.txt", "r")
-	count = int(f.read())
-	f.close()
+    f = open("count.txt", "r")
+    count = int(f.read())
+    f.close()
 	
 	# Increment the count
-	count += 1
+    count += 1
 
 	# Overwrite the count
-	f = open("count.txt", "w")
-	f.write(str(count))
+    f = open("count.txt", "w")
+    f.write(str(count))
     f.close()
 
     userName = 'davekatz'
