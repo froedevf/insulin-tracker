01ca56a19946e78c304a4b4fc5ca66d555dd5788
froedevf <david_l_katz@yahoo.com>
2021-02-22 22:58:42 -0500

    bugfix

--- app.py
+++ app.py
@@ -43,9 +43,9 @@ def activate_or_age( userName, item ):
         is_good = fresh_window > age
         timeleft = fresh_window - age
         if fresh_window < dt.timedelta(minutes = 1): # for testing only, fresh window is in seconds
-            timeleftstr = "{0} seconds".format( timeleft.total_seconds )
+            timeleftstr = "{0} seconds".format( timeleft.total_seconds() )
         else:
-            timeleftstr = "{0} days".format( timeleft.total_days )
+            timeleftstr = "{0} days".format( timeleft.total_days() )
         if is_good:
             return render_template("good.html", userName=userName, item=item, timeleft=timeleftstr, ts=act_ts )
         else:
