5a41cb61aa1f3cf726268d7203ff42b1c212d4de
froedevf <david_l_katz@yahoo.com>
2021-02-22 22:16:47 -0500

    bugfix

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import psycopg2
-import databasehelper
+from databasehelper import executeSql, query, queryUserId
 import datetime as dt
 
 app = Flask(__name__)
@@ -20,23 +20,23 @@ def activate_or_age( userName, item ):
 
     # Validate/Find the username
     try:
-        userId = databasehelper.queryUserId(userName=userName)
+        userId = queryUserId(userName=userName)
     except:
         return render_template("error.html", msg="UserName {0} not found, please register!".format(userName))
 
     # Find out if vial is already activated
     ts = dt.datetime.now()
     
-    itemCount = databasehelper.query("select count(*) from vials where user_id={0} and id={1}".format(userId, item))
+    itemCount = query("select count(*) from vials where user_id={0} and id={1}".format(userId, item))
     if itemCount == 0:
         # Activate!
         sql = "INSERT INTO vials (id, user_id, counter_start, checks) values ({0},{1},{2},{3})".format( item, userId, ts, 0 )
-        databasehelper.executeSql(sql)
+        executeSql(sql)
         return render_template("Activate.html", userName=userName, item=item, ts=ts )
     else:
         # Check!
         sql = "SELECT counter_start FROM vials WHERE user_id={0} and id={1}".format(userId, item)
-        act_ts = databasehelper.query(sql)
+        act_ts = query(sql)
         age = act_ts - ts
         daysleft = 28 - age.days()
         if dt.timedelta(days=28) > age:
@@ -47,7 +47,7 @@ def activate_or_age( userName, item ):
 
 def reset( userName, item ):
     sql = "DELETE * FROM vials WHERE user_id='{0}' and id='{1}'".format(userId, item)
-    databasehelper.executeSql(sql)
+    executeSql(sql)
     return render_template("Reset.html", userName=userName, item=item, ts=dt.datetime.now() )
     
 if __name__ == "__main__":
