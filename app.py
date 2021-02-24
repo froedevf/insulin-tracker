912a150a2517386ddd62a4e2e142786a204b22b3
froedevf <david_l_katz@yahoo.com>
2021-02-23 20:21:58 -0500

    Bug fix

--- app.py
+++ app.py
@@ -60,8 +60,5 @@ def reset( userName, item ):
     executeSql(sql)
     return render_template("reset.html", userName=userName, item=item, ts=dt.datetime.now() )
 
-
-if __name__ == "__main__":
-app.run(host='0.0.0.0', port=5001)
 if __name__ == "__main__":
 	app.run(debug=True)
