58a8710dec8a665f1c7bbbd6c4232dae08e4b710
froedevf <david_l_katz@yahoo.com>
2021-02-23 20:48:20 -0500

    Bug fix

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Markup
 import psycopg2
 from databasehelper import executeSql, query, queryUserId
 import datetime as dt
